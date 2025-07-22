8fc0a43c04874acaa7f9552e5272e23b2c87625b
Faniyang <yyifan777@gmail.com>
2025-07-22 18:37:43 +0800

    Problems#1 Two Sum - Easy

---
+++ src/problems/mod.rs
@@ -0,0 +1,1 @@
+pub mod q_0001_two_sum;
---
+++ src/problems/q_0001_two_sum.rs
@@ -0,0 +1,51 @@
+/**
+ * 1. Two Sum - Easy
+ *
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ */
+
+/**
+ * 遍历数组 nums，计算与当前数字 num 互补的数 complement
+ * 之后检查哈希表里是否包含 complement，若有则返回对应索引，否则则将当前数字及索引存入哈希表，继续遍历
+ */
+
+pub struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map: HashMap<i32, i32> = HashMap::new();
+
+        for (i, num) in nums.iter().enumerate() {
+            let complement = target - num;
+            if let Some(&j) = map.get(&complement) {
+                return vec![j, i as i32];
+            }
+            map.insert(*num, i as i32);
+        }
+
+        vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
+        assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![3, 3], 6));
+    }
+}
